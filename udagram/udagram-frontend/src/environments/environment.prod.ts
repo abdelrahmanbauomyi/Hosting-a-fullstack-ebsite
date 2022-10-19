c128997ad5f42274c96499675aaab7356ae49266
Abdelrahman Bauomyi <abdo01820@gmail.com>
2022-10-19 11:43:13 +0200

    small changes

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udacityproject-env.eba-2ap8nkup.us-east-1.elasticbeanstalk.com/',
 };
 
 /*
