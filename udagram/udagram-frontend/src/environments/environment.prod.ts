6c795bf65f466fc1902dca1090d8e2c18805d0d4
Abdelrahman Bauomyi <abdo01820@gmail.com>
2022-10-19 17:01:27 +0200

    fix front end

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://http://udacityproject.eba-2ap8nkup.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udacityproject.eba-2ap8nkup.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://http://udacityproject.eba-2ap8nkup.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udacityproject.eba-2ap8nkup.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
