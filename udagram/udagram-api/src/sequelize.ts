275ef4713cbd8ffb22fcb77551ef9c4970ec82f7
Abdelrahman Bauomyi <abdo01820@gmail.com>
2022-10-19 16:21:09 +0200

    fixing the sequlize ethod

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port:config.port as unknown as number,
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`);
+// {
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port:config.port as unknown as number,
+//   dialect: "postgres",
+//   storage: ":memory:",
+// }
